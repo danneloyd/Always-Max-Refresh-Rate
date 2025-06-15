6f634420f90d0f5a1dac8db4013be77ba4ad3724
Daniils Rimars <danneloyd@gmail.com>
2025-06-15 03:28:09 +0300

    fix

--- 120HZ/service.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-settings put system min_refresh_rate 120.0
-settings put system peak_refresh_rate 120.0
-
--- 90HZ/service.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-settings put system min_refresh_rate 90.0
-settings put system peak_refresh_rate 90.0
-
